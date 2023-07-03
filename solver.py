5877da4576acf94118ad38f493e830ec29fff1af
Devin Jean <emailcruzjean@yahoo.com>
2023-07-03 14:17:52 -0500

    rooms and 2x2 halls

--- solver.py
+++ solver.py
@@ -1,7 +1,7 @@
 import sys
 import z3
 
-SPACE = 0
+HALL = 0
 ROOM = 1
 WALL = 2
 CHEST = 3
@@ -14,7 +14,7 @@ PARSER = {
     'm': MONSTER,
 }
 DISPLAY = {
-    SPACE: '  ',
+    HALL: '  ',
     ROOM: '⭐',
     WALL: '🧱',
     CHEST: '🏆',
@@ -42,10 +42,12 @@ def main():
     def make_state(name, val):
         v = z3.Int(name)
         if val is None:
-            s.add(z3.Or(v == SPACE, v == ROOM, v == WALL))
+            s.add(z3.Or(v == HALL, v == ROOM, v == WALL))
         else:
             s.add(v == val)
         return v
+
+    # create all the tiles - pad the borders with out-of-bounds walls
     sol = { (i, j): make_state(f'v{i},{j}', board[i][j]) for j in range(num_cols) for i in range(num_rows) }
     for i in range(num_rows):
         sol[(i, -1)] = sol[(i, num_cols)] = WALL
@@ -53,19 +55,19 @@ def main():
         sol[(-1, j)] = sol[(num_rows, j)] = WALL
     assert len(sol) == (num_rows + 2) * (num_cols + 2) - 4
 
+    # make sure we satisfy the hints for number of walls in rows/cols
     for i in range(num_rows):
         s.add(sum(z3.If(sol[(i, j)] == WALL, 1, 0) for j in range(num_cols)) == row_hints[i])
     for j in range(num_cols):
         s.add(sum(z3.If(sol[(i, j)] == WALL, 1, 0) for i in range(num_rows)) == col_hints[j])
 
+    # all non-walls must form a single connected component (connected from a single arbitrary known non-wall)
     P, mkP, _ = z3.TupleSort('P', [z3.IntSort(), z3.IntSort()])
     reach = z3.Function('r', P, P, z3.BoolSort())
     reach_tc = z3.TransitiveClosure(reach)
-
     dum = z3.Ints('dumi dumj dumx dumy')
     out_of_bounds = z3.Or(dum[0] < 0, dum[0] >= num_rows, dum[1] < 0, dum[1] >= num_cols, dum[2] < 0, dum[2] >= num_rows, dum[3] < 0, dum[3] >= num_cols)
     s.add(z3.ForAll(dum, z3.Implies(out_of_bounds, z3.Not(reach(mkP(dum[0], dum[1]), mkP(dum[2], dum[3]))))))
-
     for i in range(num_rows):
         for j in range(num_cols):
             p = (i, j)
@@ -81,13 +83,36 @@ def main():
                         s.add(z3.Not(reach(mkP(p), mkP(q))))
             s.add(z3.Or(sol[p] == WALL, reach_tc(mkP(knowns[0]), mkP(p))))
 
-    deg = {}
+    # dead end (degree 1) iff monster
     for i in range(num_rows):
         for j in range(num_cols):
             p = (i, j)
             adj = [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]
-            deg[p] = sum(z3.If(sol[q] != WALL, 1, 0) for q in adj)
-            s.add(z3.Implies(sol[p] != WALL, (deg[p] == 1) == (sol[p] == MONSTER)))
+            deg = sum(z3.If(sol[q] != WALL, 1, 0) for q in adj)
+            s.add(z3.Implies(sol[p] != WALL, (deg == 1) == (sol[p] == MONSTER)))
+
+    # no 2x2 hallway subgraphs
+    for i in range(1, num_rows):
+        for j in range(1, num_cols):
+            group = [(i - 1, j - 1), (i - 1, j), (i, j - 1), (i, j)]
+            s.add(z3.Implies(z3.And(*[sol[p] != WALL for p in group]), z3.And(*[sol[p] != HALL for p in group])))
+
+    # treasure rooms
+    for chest in chest_pos:
+        interiors = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]
+        borders = [(-2, -1), (-2, 0), (-2, 1), (-1, -2), (-1, 2), (0, -2), (0, 2), (1, -2), (1, 2), (2, -1), (2, 0), (2, 1)]
+        options = []
+        for delta in interiors:
+            center = (chest[0] + delta[0], chest[1] + delta[1])
+            if center[0] < 1 or center[0] >= num_rows - 1 or center[1] < 1 or center[1] >= num_cols - 1:
+                continue
+            is_3x3 = z3.And(*[sol[(center[0] + dx, center[1] + dy)] != WALL for dx, dy in interiors])
+            is_enclosed = sum(z3.If(sol[(center[0] + dx, center[1] + dy)] == WALL, 1, 0) for dx, dy in borders) == 11
+            options.append(z3.And(is_3x3, is_enclosed))
+        s.add(z3.Or(*options))
+
+    # decide room/hall for empty spaces
+    s.add(sum(z3.If(sol[(i, j)] == ROOM, 1, 0) for i in range(num_rows) for j in range(num_cols)) == 8 * len(chest_pos))
 
     res = s.check()
     if res == z3.sat:
